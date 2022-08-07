a8cb81fb1285f5b210876d72b550d8924c8a5520
jonkahana <jonkahana1996@gmail.com>
2022-08-07 13:33:43 +0300

    dtd training

---
+++ phoenix/iNet/train_vit_b.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /cs/labs/yedid/jonkahana/projects/zero_shot/MUST
+
+source /cs/labs/yedid/jonkahana/lord_cl_venv/bin/activate.csh
+module load tensorflow/2.5.0
+export CUDA_HOME="/usr/local/nvidia/cuda/11.0/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64"
+
+
+python -u train.py --dataset imagenet --clip_model ViT-B/16
+
